fa4d3cf79b9f2b8bb4a84ef2e1b60443471bfb70
alshaibaniA <AlshaibaniAhmedA@gmail.com>
2024-03-30 06:01:02 +0300

    handle error occur when filtering data by date

--- expolor_data.py
+++ expolor_data.py
@@ -46,226 +46,231 @@ def show_page():
 	    ]
 	else:
 	    df = df.copy()
-	st.write(
-		"""
 
-		### display pie chart
+	if df.empty:
+		st.write("no data available for these dates")
+	else:
 
-		""")
-	data = df["Items"].value_counts()
-	fig1, ax1 = plt.subplots()
-	ax1.pie(data,labels=data.index,autopct="1.1f%%",shadow=True, startangle=90)
-	ax1.axis("equal")
-
-	st.write("""### number of data of bakery store""")
-	st.pyplot(fig1)
-
-	st.write(
-
+		st.write(
 			"""
-				### bar chart
-			"""
-		)
 
-	# Assuming you have a DataFrame named 'df' with 'Items' and 'Daypart' columns
-	data = df.groupby(['Daypart', 'Items']).size().unstack()
+			### display pie chart
 
-	# Create a grouped bar chart
-	fig, ax = plt.subplots()
-	data.plot(kind='bar', ax=ax)
+			""")
+		data = df["Items"].value_counts()
+		fig1, ax1 = plt.subplots()
+		ax1.pie(data,labels=data.index,autopct="1.1f%%",shadow=True, startangle=90)
+		ax1.axis("equal")
 
-	# Set labels and title
-	ax.set_xlabel('Daypart')
-	ax.set_ylabel('Count')
-	ax.set_title('Distribution of Items by Daypart')
+		st.write("""### number of data of bakery store""")
+		st.pyplot(fig1)
 
-	# Display the chart in Streamlit
-	st.pyplot(fig)
+		st.write(
 
-	st.write(
+				"""
+					### bar chart
+				"""
+			)
 
-			"""
-				### most purchesed items in each day part
-			"""
-		)
+		# Assuming you have a DataFrame named 'df' with 'Items' and 'Daypart' columns
+		data = df.groupby(['Daypart', 'Items']).size().unstack()
 
-	# Assuming you have a DataFrame named 'df' with columns: 'Items' and 'Daypart'
-	data = df.groupby(['Daypart', 'Items']).size().reset_index(name='Count')
-	max_items = data.groupby('Daypart')['Count'].idxmax()
-	most_purchased_items = data.loc[max_items]
+		# Create a grouped bar chart
+		fig, ax = plt.subplots()
+		data.plot(kind='bar', ax=ax)
 
-	# Create a bar chart
-	fig, ax = plt.subplots()
-	ax.bar(most_purchased_items['Daypart'], most_purchased_items['Items'])
+		# Set labels and title
+		ax.set_xlabel('Daypart')
+		ax.set_ylabel('Count')
+		ax.set_title('Distribution of Items by Daypart')
 
-	# Set labels and title
-	ax.set_xlabel('Daypart')
-	ax.set_ylabel('Most Purchased Item')
-	ax.set_title('Most Purchased Item in Each Daypart')
+		# Display the chart in Streamlit
+		st.pyplot(fig)
 
-	# Display the chart in Streamlit
-	st.pyplot(fig)
+		st.write(
 
-	st.write(
+				"""
+					### most purchesed items in each day part
+				"""
+			)
 
-			"""
-				### display each item how many times it has been purchesed in each part of the day
-			"""
-		)
+		# Assuming you have a DataFrame named 'df' with columns: 'Items' and 'Daypart'
+		data = df.groupby(['Daypart', 'Items']).size().reset_index(name='Count')
+		max_items = data.groupby('Daypart')['Count'].idxmax()
+		most_purchased_items = data.loc[max_items]
 
-	# Assuming you have a DataFrame named 'df' with columns: 'Items' and 'Daypart'
-	pivot_table = pd.pivot_table(df, index='Items', columns='Daypart', aggfunc='size', fill_value=0)
+		# Create a bar chart
+		fig, ax = plt.subplots()
+		ax.bar(most_purchased_items['Daypart'], most_purchased_items['Items'])
 
-	# Display the pivot table in Streamlit
-	st.write(pivot_table)
+		# Set labels and title
+		ax.set_xlabel('Daypart')
+		ax.set_ylabel('Most Purchased Item')
+		ax.set_title('Most Purchased Item in Each Daypart')
 
-	st.write(
+		# Display the chart in Streamlit
+		st.pyplot(fig)
 
-			"""
-				### number of purchese of each item based on time
-			"""
-		)
-	# Group DataFrame by time and calculate the count of items
-	time_counts = df.groupby('Hour')['Items'].count()
+		st.write(
 
-	# Create a sequential chart
-	fig, ax = plt.subplots()
-	line=ax.plot(time_counts.index, time_counts.values)
+				"""
+					### display each item how many times it has been purchesed in each part of the day
+				"""
+			)
 
-	# Set labels and title
-	ax.set_xlabel('Hour')
-	ax.set_ylabel('Number of Items Purchased')
-	ax.set_title('Sequential Chart of Items Purchased over Hour')
+		# Assuming you have a DataFrame named 'df' with columns: 'Items' and 'Daypart'
+		pivot_table = pd.pivot_table(df, index='Items', columns='Daypart', aggfunc='size', fill_value=0)
 
-	mplcursors.cursor(line)
-	#mpldatacursor.datacursor(hover=True)
+		# Display the pivot table in Streamlit
+		st.write(pivot_table)
 
+		st.write(
 
-	# Display the chart in Streamlit
-	st.pyplot(fig)
+				"""
+					### number of purchese of each item based on time
+				"""
+			)
+		# Group DataFrame by time and calculate the count of items
+		time_counts = df.groupby('Hour')['Items'].count()
 
+		# Create a sequential chart
+		fig, ax = plt.subplots()
+		line=ax.plot(time_counts.index, time_counts.values)
 
+		# Set labels and title
+		ax.set_xlabel('Hour')
+		ax.set_ylabel('Number of Items Purchased')
+		ax.set_title('Sequential Chart of Items Purchased over Hour')
 
-	st.write(
-
-			"""
-				### is it working?
-			"""
-		)
-
-	# Group DataFrame by hour and calculate the count of items
-	hour_counts = df.groupby('Hour')['Items'].count().reset_index()
-
-	# Create a sequential chart using Plotly
-	fig = px.line(hour_counts, x='Hour', y='Items', labels={'Hour': 'Hour', 'Items': 'Number of Items Purchased'},
-	              title='Sequential Chart of Items Purchased by Hour')
-
-	# Enable hover information
-	fig.update_traces(hovertemplate='Hour: %{x}<br>Number of Items Purchased: %{y}')
-
-	# Display the chart in Streamlit
-	st.plotly_chart(fig)
+		mplcursors.cursor(line)
+		#mpldatacursor.datacursor(hover=True)
 
 
-	st.write(
-
-			"""
-				### Lets try by days of the week?
-			"""
-		)
-
-	# Group DataFrame by hour and calculate the count of items
-	hour_counts = df.groupby('DayOfWeek')['Items'].count().reset_index()
-
-	# Create a sequential chart using Plotly
-	fig = px.line(hour_counts, x='DayOfWeek', y='Items', labels={'DayOfWeek': 'DayOfWeek', 'Items': 'Number of Items Purchased'},
-	              title='Sequential Chart of Items Purchased by Day Of Week')
-
-	# Enable hover information
-	fig.update_traces(hovertemplate='DayOfWeek: %{x}<br>Number of Items Purchased: %{y}')
-
-	# Display the chart in Streamlit
-	st.plotly_chart(fig)
-
-
-	st.write(
-
-			"""
-				### Heatmap
-			"""
-		)
-	# Group DataFrame by day of the week and hour, and calculate the count of items
-	purchases_by_time = df.groupby(['DayOfWeek', df['DateTime'].dt.hour])['Items'].count().unstack()
-
-	# Create a heatmap
-	fig, ax = plt.subplots(figsize=(10, 6))
-	sns.heatmap(purchases_by_time, cmap='YlOrRd', ax=ax, annot=True, fmt='g')
-
-	# Set labels and title
-	ax.set_xlabel('Hour of the Day')
-	ax.set_ylabel('Day of the Week')
-	ax.set_title('Number of Purchases by Time and Day of the Week')
-
-	# Adjust the layout to prevent cutoff of labels
-	plt.tight_layout()
-
-	# Display the chart in Streamlit
-	st.pyplot(fig)
+		# Display the chart in Streamlit
+		st.pyplot(fig)
 
 
 
+		st.write(
 
-	st.write(
+				"""
+					### is it working?
+				"""
+			)
 
-			"""
-				### number of purchese that occur in each single day
-			"""
-		)
-	# Assuming you have a DataFrame named 'df' with a 'Date' column
-	df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+		# Group DataFrame by hour and calculate the count of items
+		hour_counts = df.groupby('Hour')['Items'].count().reset_index()
 
-	# Group DataFrame by date and calculate the count of purchases
-	daily_purchases = df.groupby(df['Date'].dt.date)['Items'].count()
+		# Create a sequential chart using Plotly
+		fig = px.line(hour_counts, x='Hour', y='Items', labels={'Hour': 'Hour', 'Items': 'Number of Items Purchased'},
+		              title='Sequential Chart of Items Purchased by Hour')
 
-	# Create a line chart
-	fig, ax = plt.subplots()
-	line, =ax.plot(daily_purchases.index, daily_purchases.values)
+		# Enable hover information
+		fig.update_traces(hovertemplate='Hour: %{x}<br>Number of Items Purchased: %{y}')
 
-	# Set labels and title
-	ax.set_xlabel('Date')
-	ax.set_ylabel('Number of Purchases')
-	ax.set_title('Purchases by Date')
-
-	# Rotate x-axis labels for better readability
-	plt.xticks(rotation=45)
-
-	# Create the cursor tooltip
-	mplcursors.cursor(hover=True).connect("add", lambda sel: sel.annotation.set_text(f"{sel.target[0]}, {sel.target[1]}"))
-	# Display the chart in Streamlit
-	st.pyplot(fig)
+		# Display the chart in Streamlit
+		st.plotly_chart(fig)
 
 
-	st.write(
+		st.write(
 
-			"""
-				### Enable cursor
-			"""
-		)
-	# Assuming you have a DataFrame named 'df' with a 'Date' column
-	df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+				"""
+					### Lets try by days of the week?
+				"""
+			)
 
-	# Group DataFrame by date and calculate the count of purchases
-	daily_purchases = df.groupby(df['Date'].dt.date)['Items'].count().reset_index()
+		# Group DataFrame by hour and calculate the count of items
+		hour_counts = df.groupby('DayOfWeek')['Items'].count().reset_index()
 
-	# Create a line chart with hover tooltips
-	fig = px.line(daily_purchases, x='Date', y='Items', labels={'Date': 'Date', 'Items': 'Number of Purchases'},
-	              title='Purchases by Date')
-	fig.update_xaxes(tickangle=45)
+		# Create a sequential chart using Plotly
+		fig = px.line(hour_counts, x='DayOfWeek', y='Items', labels={'DayOfWeek': 'DayOfWeek', 'Items': 'Number of Items Purchased'},
+		              title='Sequential Chart of Items Purchased by Day Of Week')
 
-	# Enable hover tooltips
-	fig.update_traces(hovertemplate='Date: %{x}<br>Number of Purchases: %{y}')
+		# Enable hover information
+		fig.update_traces(hovertemplate='DayOfWeek: %{x}<br>Number of Items Purchased: %{y}')
 
-	# Display the chart in Streamlit
-	st.plotly_chart(fig)
+		# Display the chart in Streamlit
+		st.plotly_chart(fig)
 
-	st.experimental_rerun()
+
+		st.write(
+
+				"""
+					### Heatmap
+				"""
+			)
+		# Group DataFrame by day of the week and hour, and calculate the count of items
+		purchases_by_time = df.groupby(['DayOfWeek', df['DateTime'].dt.hour])['Items'].count().unstack()
+
+		# Create a heatmap
+		fig, ax = plt.subplots(figsize=(10, 6))
+		sns.heatmap(purchases_by_time, cmap='YlOrRd', ax=ax, annot=True, fmt='g')
+
+		# Set labels and title
+		ax.set_xlabel('Hour of the Day')
+		ax.set_ylabel('Day of the Week')
+		ax.set_title('Number of Purchases by Time and Day of the Week')
+
+		# Adjust the layout to prevent cutoff of labels
+		plt.tight_layout()
+
+		# Display the chart in Streamlit
+		st.pyplot(fig)
+
+
+
+
+		st.write(
+
+				"""
+					### number of purchese that occur in each single day
+				"""
+			)
+		# Assuming you have a DataFrame named 'df' with a 'Date' column
+		df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+
+		# Group DataFrame by date and calculate the count of purchases
+		daily_purchases = df.groupby(df['Date'].dt.date)['Items'].count()
+
+		# Create a line chart
+		fig, ax = plt.subplots()
+		line, =ax.plot(daily_purchases.index, daily_purchases.values)
+
+		# Set labels and title
+		ax.set_xlabel('Date')
+		ax.set_ylabel('Number of Purchases')
+		ax.set_title('Purchases by Date')
+
+		# Rotate x-axis labels for better readability
+		plt.xticks(rotation=45)
+
+		# Create the cursor tooltip
+		mplcursors.cursor(hover=True).connect("add", lambda sel: sel.annotation.set_text(f"{sel.target[0]}, {sel.target[1]}"))
+		# Display the chart in Streamlit
+		st.pyplot(fig)
+
+
+		st.write(
+
+				"""
+					### Enable cursor
+				"""
+			)
+		# Assuming you have a DataFrame named 'df' with a 'Date' column
+		df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+
+		# Group DataFrame by date and calculate the count of purchases
+		daily_purchases = df.groupby(df['Date'].dt.date)['Items'].count().reset_index()
+
+		# Create a line chart with hover tooltips
+		fig = px.line(daily_purchases, x='Date', y='Items', labels={'Date': 'Date', 'Items': 'Number of Purchases'},
+		              title='Purchases by Date')
+		fig.update_xaxes(tickangle=45)
+
+		# Enable hover tooltips
+		fig.update_traces(hovertemplate='Date: %{x}<br>Number of Purchases: %{y}')
+
+		# Display the chart in Streamlit
+		st.plotly_chart(fig)
+
+		st.experimental_rerun()
